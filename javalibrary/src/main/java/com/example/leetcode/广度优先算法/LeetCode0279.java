e7052accb23ed7b11aed06b969d225a5da3e2a01
tianhao <tianhao@cmcm.com>
2020-05-06 17:21:09 +0800

    深度优先+动态规划

--- javalibrary/src/main/java/com/example/leetcode/广度优先算法/LeetCode0279.java
+++ javalibrary/src/main/java/com/example/leetcode/广度优先算法/LeetCode0279.java
@@ -1,21 +1,23 @@
 package com.example.leetcode.广度优先算法;
 
 import java.util.Arrays;
+import java.util.HashSet;
+import java.util.Set;
 
 public class LeetCode0279 {
 
         public static void main(String [] args) {
-            System.out.println(numSquares(12));
+            System.out.println(numSquares1(12));
         }
-        public static int numSquares(int n) {
-            int dp[] = new int[n + 1];
+         private static int numSquares1(int n) {
+            int[] dp = new int[n + 1];
             Arrays.fill(dp, Integer.MAX_VALUE);
             // bottom case
             dp[0] = 0;
 
             // pre-calculate the square numbers.
             int max_square_index = (int) Math.sqrt(n) + 1;
-            int square_nums[] = new int[max_square_index];
+            int[] square_nums = new int[max_square_index];
             for (int i = 1; i < max_square_index; ++i) {
                 square_nums[i] = i * i;
             }
@@ -29,4 +31,35 @@ public class LeetCode0279 {
             }
             return dp[n];
         }
+
+    static Set<Integer> square_nums = new HashSet<Integer>();
+
+    protected static boolean is_divided_by(int n, int count) {
+        if (count == 1) {
+            return square_nums.contains(n);
+        }
+
+        for (Integer square : square_nums) {
+            if (is_divided_by(n - square, count - 1)) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    public static int numSquares2(int n) {
+        square_nums.clear();
+
+        for (int i = 1; i * i <= n; ++i) {
+            square_nums.add(i * i);
+        }
+
+        int count = 1;
+        for (; count <= n; ++count) {
+            if (is_divided_by(n, count))
+                return count;
+        }
+        return count;
+    }
+
 }
